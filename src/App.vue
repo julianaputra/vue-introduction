f8f0185df8100bd988f0581bf01781d40eb1be4b
Juliana Putra <julianaputra10@gmail.com>
2024-01-13 15:16:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="todo-app">
+    <header>
+      <h1 class="title">Todo App</h1>
+      <form @submit.prevent="createTodo">
+        <div class="input-group">
+          <input class="todo-input" v-model="todoInput">
+          <button type="submit">Add</button>
+        </div>
+      </form>
+    </header>
+
+    <div>
+      <TodoItem v-for="(todo, index) in todoList" 
+      :todo="todo" 
+      :index="index"
+      @delete="removeTodo"
+       />
+    </div>
+
+    <div v-if="todoList.length < 1">
+      <h4>Belum ada data</h4>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import TodoItem from '@/components/TodoItem.vue'
+
+const todoInput = ref('')
+const todoList = ref([])
+
+const createTodo = () => {
+  todoList.value.push(todoInput.value)
+  todoInput.value = ''
+}
+
+const removeTodo = (index) => {
+  todoList.value.splice(index, 1);
+}
+</script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="todo-item">
+        <h3>{{ todo }}</h3>
+        <button @click="deleteTodo(index)">Delete</button>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    todo: {
+        type: String,
+        required: true
+    },
+    index: {
+        type: Number,
+        required: true
+    }
+})
+
+const emit = defineEmits(['delete'])
+
+const deleteTodo = (index) => {
+    emit('delete', index)
+}
+</script>
